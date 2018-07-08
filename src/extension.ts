92d780ab1741d285f417f79359992d299a7306cc
Kilari Teja <kilariteja9@gmail.com>
2018-07-08 18:26:36 +0530

    Add lint & fix typescript linting issues
    Fix the linting issues reported by coala (tslint).

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,76 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 'use strict';
 
 import * as net from 'net';
 
-import { workspace, Disposable, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, ErrorAction, ErrorHandler, CloseAction, TransportKind } from 'vscode-languageclient';
+import { Disposable, ExtensionContext, workspace } from 'vscode';
+import {
+    CloseAction,
+    ErrorAction,
+    ErrorHandler,
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    SettingMonitor,
+    TransportKind,
+} from 'vscode-languageclient';
 
-function startLangServer(command: string, documentSelector: string | string[]): Disposable {
-	const serverOptions: ServerOptions = {
-		command: command,
-	};
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: documentSelector,
-	}
-	return new LanguageClient(command, serverOptions, clientOptions).start();
+function startLangServer(
+    command: string,
+    documentSelector: string | string[]
+): Disposable {
+    const serverOptions: ServerOptions = {
+        command: command,
+    };
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: documentSelector,
+    };
+    return new LanguageClient(
+        'coala langserver', serverOptions, clientOptions).start();
 }
 
-function startLangServerTCP(addr: number, documentSelector: string | string[]): Disposable {
-	const serverOptions: ServerOptions = function() {
-		return new Promise((resolve, reject) => {
-			var client = new net.Socket();
-			client.connect(addr, "127.0.0.1", function() {
-				resolve({
-					reader: client,
-					writer: client
-				});
-			});
-		});
-	}
+function startLangServerTCP(
+    addr: number,
+    documentSelector: string | string[]
+): Disposable {
+    const serverOptions: ServerOptions = () => {
+        return new Promise((resolve, reject) => {
+            const client = new net.Socket();
+            client.connect(
+                addr,
+                '127.0.0.1',
+                () => {
+                    resolve({
+                        reader: client,
+                        writer: client,
+                    });
+                }
+            );
+        });
+    };
 
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: documentSelector,
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.py')
-		}
-	}
-	return new LanguageClient(`tcp lang server (port ${addr})`, serverOptions, clientOptions).start();
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: documentSelector,
+        synchronize: {
+            // Notify the server about file changes to '.clientrc
+            // files contain in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/.py'),
+        },
+    };
+    return new LanguageClient(
+        `tcp lang server (port ${addr})`,
+        serverOptions,
+        clientOptions
+    ).start();
 }
 
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(startLangServer
-	(require("path").resolve(__dirname, '../coala-langserver.sh'), ["python"]));
-	// For Debug
-	// context.subscriptions.push(startLangServerTCP(2087, ["python"]));
-	console.log("coala language server is running.");
+    context.subscriptions.push(
+        startLangServer(
+            require('path').resolve(__dirname, '../coala-langserver.sh'),
+            ['python']
+        )
+    );
+    // For Debug
+    // context.subscriptions.push(startLangServerTCP(2087, ['python']));
+    console.log('coala language server is running.');
 }
