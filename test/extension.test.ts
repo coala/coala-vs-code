e18fe3ffcdf187bba4f488fbaca3d4ade97b1e60
Kilari Teja <kilariteja9@gmail.com>
2018-07-09 02:44:11 +0530

    extension.test.ts: Add extension test
    Add a test to the extension.

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,5 +1,6 @@
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+const activate = require('../src/extension');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -10,8 +11,7 @@ import * as assert from 'assert';
 suite('Extension Tests', () => {
 
     // Defines a Mocha unit test
-    test('Something 1', () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test('Import Extension', () => {
+        assert.notEqual(activate, null);
     });
 });
